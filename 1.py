7b30e81021b083745cdc03defddc72808b8f248e
Mansi Pachauri <132356452+mansipachauri@users.noreply.github.com>
2023-06-05 17:29:09 +0530

    commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+print("hello world")
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4                                                      # x is of type int
+x = "Sally"                                                # x is now of type str
+print(x)
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
